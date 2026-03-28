183404bd97027781465aca13bdcb3183e2dee1e8
kalirajan1129 <kalirajan1129@gmail.com>
2026-03-28 12:19:47 +0530

    Initial project setup with backend server and basic structure

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

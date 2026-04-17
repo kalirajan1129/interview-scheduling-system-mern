9c5de308c4221276d0b9180ebe5fb177613b1b72
kalirajan1129 <kalirajan1129@gmail.com>
2026-04-17 14:26:22 +0530

    Added edit and delete interview features with UI improvements and final polish

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
 //   baseURL: 'http://localhost:5000/api'
-     baseUrl: 'https://interview-scheduling-system-mern.onrender.com'
+     baseUrl: 'https://interview-scheduling-system-mern.onrender.com/api'
 });
 
 // 🔥 Attach token automatically

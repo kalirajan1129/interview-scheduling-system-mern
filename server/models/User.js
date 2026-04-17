27b628893e85c2c49683eec194695dd8362419cf
kalirajan1129 <kalirajan1129@gmail.com>
2026-04-17 12:51:36 +0530

    Added frontend authentication with login page, JWT handling, and protected routes

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,5 +22,11 @@ const userSchema = new mongoose.Schema({
     default: 'HR'
   }
 }, { timestamps: true });
+// before save
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = mongoose.model('User', userSchema);

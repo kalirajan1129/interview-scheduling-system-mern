01c133157cff8f0b1ad9953e4c651bdedb9956d9
kalirajan1129 <kalirajan1129@gmail.com>
2026-04-17 16:45:09 +0530

    Updated Email Features  for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-   baseURL: 'http://localhost:5000/api'
-    // baseURL: 'https://interview-scheduling-system-mern.onrender.com/api'
+   //baseURL: 'http://localhost:5000/api'
+     baseURL: 'https://interview-scheduling-system-mern.onrender.com/api'
 });
 
 // 🔥 Attach token automatically
